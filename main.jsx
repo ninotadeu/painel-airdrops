9a51afd070ecc35b8700f28e23e3e824db6c42f6
ninotadeu <nino.tadeu@proton.me>
2025-04-04 19:29:33 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import PainelAirdrops from './PainelAirdrops'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PainelAirdrops />
+  </React.StrictMode>,
+)
